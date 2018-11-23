0788688eff0a05f75d15b85e516b4f7b3dd26ca2
Parksohui <201802094@o.cnu.ac.kr>
2018-11-23 20:24:02 +0900

    no message

--- Calculator.java
+++ Calculator.java
@@ -7,6 +7,9 @@ public class Calculator(){
   public int minus(int a, int b){
     return a-b;
   }
+  public int multiple(int a, int b){
+    return a*b;
+  }
 
   public static void main(String [] args){
     int a = 4;
